44989853a42d5c4d4b58c0aefe5ef238fba74589
Alice Gimenez <alicegimenezsiq@gmail.com>
2026-08-30 22:00:50 -0300

    inserindo código do rodapé

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+    </main>
+    <footer class="rodape">
+        <p>&copy; <?= date('Y') ?> Café Lumière — Av. Macedo Soares, 479, Capivari, Campos do Jordão - SP</p>
+        <p>contato@cafelumiere.com | @cafelumiere</p>
+    </footer>
+</body>
+</html>
