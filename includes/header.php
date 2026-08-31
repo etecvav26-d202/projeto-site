f78618cd8f11403856a40f0baef73e42282acff7
Ana Lívia <analiviatakeyama2009@gmail.com>
2026-08-30 22:35:40 -0300

    corrigindo redirecionamento

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Café Lumière</title>
-    <link rel="stylesheet" href="/cafe-lumiere/css/style.css">
+    <link rel="stylesheet" href="/projeto-site/css/style.css">
 </head>
 <body>
     <header class="topo">
@@ -12,14 +12,14 @@
         <nav>
             <button class="menu-hamburguer">☰</button>
             <ul class="menu">
-                <li><a href="/cafe-lumiere/index.php">Home</a></li>
-                <li><a href="/cafe-lumiere/cardapio.php">Cardápio</a></li>
-                <li><a href="/cafe-lumiere/livros.php">Livros</a></li>
-                <li><a href="/cafe-lumiere/eventos.php">Eventos</a></li>
-                <li><a href="/cafe-lumiere/reservas.php">Reservas</a></li>
-                <li><a href="/cafe-lumiere/carrinho.php">Carrinho</a></li>
-                <li><a href="/cafe-lumiere/contato.php">Contato</a></li>
-                <li><a href="/cafe-lumiere/login.php">Login</a></li>
+                <li><a href="/projeto-site/index.php">Home</a></li>
+                <li><a href="/projeto-site/cardapio.php">Cardápio</a></li>
+                <li><a href="/projeto-site/livros.php">Livros</a></li>
+                <li><a href="/projeto-site/eventos.php">Eventos</a></li>
+                <li><a href="/projeto-site/reservas.php">Reservas</a></li>
+                <li><a href="/projeto-site/carrinho.php">Carrinho</a></li>
+                <li><a href="/projeto-site/contato.php">Contato</a></li>
+                <li><a href="/projeto-site/login.php">Login</a></li>
             </ul>
         </nav>
     </header>
