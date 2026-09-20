203d646abd7c1182579a8dc801e94df01cf5cba4
Alice Gimenez <alicegimenezsiq@gmail.com>
2026-09-20 20:18:14 -0300

    inserindo informações ao excluir

---
+++ admin/livros/excluir.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once '../../config/conexao.php';
+
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+if (!$id) {
+    header('Location: index.php');
+    exit;
+}
+
+$stmt = $pdo->prepare("
+    DELETE FROM livros
+    WHERE id = ?
+");
+
+$stmt->execute([$id]);
+
+header('Location: index.php');
+exit;
