afab1a4f303db0e4f2202986c2f63ae692388e10
Ana Lívia <analiviatakeyama2009@gmail.com>
2026-09-20 23:15:13 -0300

    finalizando correções

--- includes/header.php
+++ includes/header.php
@@ -39,11 +39,8 @@ if (session_status() === PHP_SESSION_NONE) {
         href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=Playfair+Display:ital,wght@0,400;0,500;0,600;1,400;1,500&display=swap"
         rel="stylesheet"
     >
-
-    <link
-        rel="stylesheet"
-        href="projeto-site/css/style.css"
-    >
+    
+    <link rel="stylesheet" href="/projeto-site/css/style.css">
 
 </head>
 <body>
